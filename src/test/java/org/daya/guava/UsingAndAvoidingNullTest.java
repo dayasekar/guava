5391ed97c635d0e6434f958988ac7baa89f10196
daya <daya@daya-notebook>
2017-02-26 23:05:28 +0800

    Added initial test for null

---
+++ src/test/java/org/daya/guava/UsingAndAvoidingNullTest.java
@@ -0,0 +1,14 @@
+package org.daya.guava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsingAndAvoidingNullTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
